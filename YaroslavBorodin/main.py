23b7978f0c5fad6e164fa2e4a54ea088d38db2a3
Yaroslav B <yaroslav9borodin@gmail.com>
2021-11-22 14:04:09 +0200

    task 2

--- YaroslavBorodin/main.py
+++ YaroslavBorodin/main.py
@@ -1,7 +1,11 @@
 mystring="Hello World!"
 first=mystring[0]
 last=mystring[-1]
-print(f"complete string: \"{mystring}\", first element: \"{first}\", last element: \"{last}\"")
+print(f"\n Complete string: \"{mystring}\", first element: \"{first}\", last element: \"{last}\" \n")
+
+first_num_string = "6"
+second_num_string = "7"
+print("\n Sum of two numbers: first_num + second_num = ", int(first_num_string)+int(second_num_string),"\n")
 
 
 
