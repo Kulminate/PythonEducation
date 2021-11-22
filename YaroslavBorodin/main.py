417a9a4ec8baf0bc846b88b412aca4c390d8e4f8
Yaroslav B <yaroslav9borodin@gmail.com>
2021-11-22 14:17:17 +0200

    task 3

--- YaroslavBorodin/main.py
+++ YaroslavBorodin/main.py
@@ -7,6 +7,12 @@ first_num_string = "6"
 second_num_string = "7"
 print("\n Sum of two numbers: first_num + second_num = ", int(first_num_string)+int(second_num_string),"\n")
 
+num = int(input("Enter some number "))
+print("Your number: {}".format(num))
+print("Your number squared: {}\n".format(num**2))
+
+
+
 
 
 
