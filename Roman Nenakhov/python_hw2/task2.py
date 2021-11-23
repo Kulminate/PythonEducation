6d5c4c98813f6cfc033cf349eccdc5f5e698848e
Roman <roman.nenakhov96@gmail.com>
2021-11-23 15:14:59 +0200

    python hw2 task2

--- Roman Nenakhov/python_hw2/task2.py
+++ Roman Nenakhov/python_hw2/task2.py
@@ -1,2 +1,9 @@
 # Вы принимаете от пользователя последовательность чисел, разделённых запятой.
 # Составьте список и кортеж с этими числами.
+numbers = input("Пожалуйста введите последовательность чисел через запятую: ")
+
+list_of_numbers = [int(i) for i in (numbers.split(','))]
+print("Список: ", list_of_numbers)
+
+tuple_of_numbers = tuple(list_of_numbers)
+print("Кортеж: ", tuple_of_numbers)
