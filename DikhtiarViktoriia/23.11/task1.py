8de8300dee75ff5f5618dccf9370a276820d507b
dihty <dihyarviktoriya95@gmail.com>
2021-11-23 14:36:01 +0200

    task1 of the second lesson changed

--- DikhtiarViktoriia/23.11/task1.py
+++ DikhtiarViktoriia/23.11/task1.py
@@ -2,7 +2,16 @@ list_nums = [1, 2, 3, 4, 5, 6]
 steps = int(input("Ввдите число: "))
 list2 = []
 
-for i in range(len(list_nums)):
+for i in list_nums:
     list2 = list_nums[-steps:] + list_nums[:-steps]
 print(list2)
 
+#Второй вариант:
+
+list_nums2 = [1, 2, 3, 4, 5, 6]
+steps2 = int(input("Ввдите число: "))
+
+for i in range(steps2):
+    elem = list_nums2.pop(-1)
+    list_nums2.insert(0, elem)
+print(list_nums2)
