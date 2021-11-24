6af9326f3e2a8a329ff77e8bb8eb26960b4b541f
Roman Vasilenko <romanvas314@gmail.com>
2021-11-24 12:28:36 +0200

    Task 4 completed with two itterators

---
+++ Roman Vasilenko 23.11.2021/Task_4.py
@@ -0,0 +1,26 @@
+def numbers_matched_range(dictionary, left_range, right_range):
+    result = []
+
+    for key, value in dictionary.items():
+        for index in range(left_range, right_range + 1):
+            if value == index:
+                result.append(key)
+    return result
+
+
+insert_dict = { 'a': 11,
+                'b': 12,
+                'c': 17,
+                'd': 23,
+                'e': 12
+}
+#int(input('Enter left range: '))
+left_range = 0
+right_range = 13
+print(numbers_matched_range(insert_dict, left_range, right_range))
+
+
+
+
+
+
