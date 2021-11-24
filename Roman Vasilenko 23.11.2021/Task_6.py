7d58719192250860a306fd93b687473f7e3ed560
Roman Vasilenko <romanvas314@gmail.com>
2021-11-24 13:47:31 +0200

    Task 6 completed with inputs

--- Roman Vasilenko 23.11.2021/Task_6.py
+++ Roman Vasilenko 23.11.2021/Task_6.py
@@ -2,10 +2,13 @@ example_list = ['x', 'y', 'z']
 result_string = ''
 index = 0
 
+height = int(input("Enter height: "))
+width = int(input("Enter width: "))
 
-for i in range (4):
+
+for i in range (height):
     result_string += "\n"
-    for u in range(4):
+    for u in range(width):
         result_string += example_list[index]
         index += 1
         if index > 2:
@@ -13,3 +16,8 @@ for i in range (4):
 
 
 print(result_string)
+
+
+
+
+
