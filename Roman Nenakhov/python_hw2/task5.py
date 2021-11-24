6e401861c6bbda134d3182bd8cb86d48bef0b5d4
Roman <roman.nenakhov96@gmail.com>
2021-11-24 13:42:12 +0200

    python hw2 task5

--- Roman Nenakhov/python_hw2/task5.py
+++ Roman Nenakhov/python_hw2/task5.py
@@ -1,4 +1,4 @@
-# У вас есть такой треугольник, он беcконечен
+# У вас есть такой треугольник, он бесконечен
 #               1
 #           3     5
 #        7     9    11
@@ -10,3 +10,20 @@
 #
 # 1 -->  1
 # 2 --> 3 + 5 = 8
+
+line_number = int(input("Введите номер строки: "))
+
+current_number = 1
+stop = 2
+result = list()
+
+for i in range(0, line_number):
+    result = list()
+    for column in range(1, stop):
+        print(current_number, end=' ')
+        result.append(current_number)
+        current_number += 2
+    print(" ")
+    stop += 1
+
+print("Сумма чисел указанной строки: ", sum(result))
