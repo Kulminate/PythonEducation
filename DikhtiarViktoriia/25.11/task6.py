57b16d58c996fdd53d891cb8cec42ce94d3a7946
dihty <dihyarviktoriya95@gmail.com>
2021-11-25 15:08:06 +0200

    task6 changed

--- DikhtiarViktoriia/25.11/task6.py
+++ DikhtiarViktoriia/25.11/task6.py
@@ -1,10 +1,14 @@
-
-
 def get_words(string):
-    a = [" ", ",", ".", "!", "?", " - ", ";", ")", "("]
+    # a = [" ", ",", ".", "!", "?", " - ", ";", ")", "("]
+    #
+    # for i in a:
+    #     b = string.replace(i, " ")
 
-    for i in a:
-        b = string.split("i")
-        print(b)
+    new_string2 = []
+    new_string = string.replace(",", " ").replace(".", " ").replace("!", " ").replace("?", " ").replace(" - ", " ").replace(";", " ").replace(":", " ").replace("(", " ").replace(")", " ").split(" ")
+    for elem in new_string:
+        if elem != "":
+            new_string2.append(elem)
+    print(len(new_string2))
 
 get_words("количество! слов в тексте. В тексте, jdj")
