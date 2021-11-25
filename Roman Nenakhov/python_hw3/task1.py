d528445a86625f44b74fa97aa1346a67d85f914a
Roman <roman.nenakhov96@gmail.com>
2021-11-25 13:23:11 +0200

    python hw3 task1

--- Roman Nenakhov/python_hw3/task1.py
+++ Roman Nenakhov/python_hw3/task1.py
@@ -2,3 +2,18 @@
 # первые 2 - числа, третий - операция, которая должна быть произведена над ними.
 # Если третий аргумент +, сложить их; если —, то вычесть; * — умножить;
 # / — разделить (первое на второе). В остальных случаях вернуть строку "Неизвестная операция".
+def arithmetic(x, y, operation):
+    if operation == "+":
+        return x + y
+    elif operation == "-":
+        return x - y
+    elif operation == "*":
+        return x * y
+    elif operation == "/":
+        return x / y
+    else:
+        err = "Неизвестная операция"
+        return err
+
+
+print(arithmetic(int(input()), int(input()), input()))
