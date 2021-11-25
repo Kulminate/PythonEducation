a9f3debebc0c64ae771f60d6f68430f5bd1fe1f5
Roman Vasilenko <romanvas314@gmail.com>
2021-11-25 16:03:44 +0200

    Task 7 completed

--- Roman Vasilenko 25.11.2021/Task_7.py
+++ Roman Vasilenko 25.11.2021/Task_7.py
@@ -1,6 +1,6 @@
 
 for block in range(1, 4):
-    print(end = '\n')
+    print(end='\n')
     for string in range(1, 4):
         result = eval('{} * {}'.format(string, block))
         print('{} x {} = {}'.format(string, block, result), end='\t')
