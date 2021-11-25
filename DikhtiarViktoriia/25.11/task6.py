12b5cbb439c9377a2ae63077475e96c0c1bb785d
dihty <dihyarviktoriya95@gmail.com>
2021-11-25 15:10:32 +0200

    task6

---
+++ DikhtiarViktoriia/25.11/task6.py
@@ -0,0 +1,8 @@
+def get_words(string):
+    a = [" ", ",", ".", "!", "?", " - ", ";", ")", "("]
+
+    for i in a:
+        b = string.split("i")
+        print(b)
+
+get_words("количество! слов в тексте. В тексте, jdj")
