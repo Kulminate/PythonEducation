6a802353ded0e75bf073175a103967f6aee32829
soffkom <ssofiakomanova@gmail.com>
2021-11-26 15:04:01 +0200

    task 6(2)

--- Komanova Sofia 25.11/6.py
+++ Komanova Sofia 25.11/6.py
@@ -6,11 +6,15 @@ def get_words(text):
     for i in new_list:
         if i == '  ':
             text_list = text.replace(i, ' ')
-        else: text_list = text.replace(i, '')
+        else: text_list = text.replace(i, ' ')
         text = text_list
-    text2 =text.split(' ')
-    return len(text2)
+    list_text2 =text.split(' ')
+    just_word = list()
+    for j in list_text2:
+        if j != '':
+            just_word.append(j)
+    return len(just_word)
 
 
-text = "dsad! sadad, - dsada sdad sda sdadas – dsad, dsadad. dsada, sda"
+text = "dsad! sadad, - dsada sdad sda sdadas – dsad, dsa.dad. dsada, sda"
 print(get_words(text))
