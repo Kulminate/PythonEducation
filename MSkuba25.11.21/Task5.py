a818817ff99c87be21a60243a711fed917211d9b
SebPereira <skuba.m.work@gmail.com>
2021-11-26 10:58:55 +0200

    task 5

---
+++ MSkuba25.11.21/Task5.py
@@ -0,0 +1,13 @@
+# 5) Написать функцию is_prime, принимающую 1 аргумент — число от 0 до 1000,
+# и возвращающую True, если оно простое, и False - иначе.
+def is_prime(numb=int(input("input numb 0 to 1000"))):
+
+    if numb > 1:
+        return "true"
+    elif numb % 1:
+        return "true"
+    elif numb % numb:
+        return "true"
+    else:
+        return "false"
+print(is_prime())
