a3390ebf1143f5762fbd44def364e006ab31a4cd
soffkom <ssofiakomanova@gmail.com>
2021-11-26 14:52:24 +0200

    task 6(2)

--- Komanova Sofia 25.11/6.py
+++ Komanova Sofia 25.11/6.py
@@ -1,10 +1,15 @@
 # 6) Написать функцию get_words(), которая подсчитывает количество слов в тексте.
 # (В тексте может быть несколько предложений со знаками пунктуации)
+
 def get_words(text):
-    text_list = text.split(' ')
-    text_list.remove('-')
-    text_list.remove('–')
-    return len(text_list)
+    new_list = [',', '.', ':', '-', '–', '?', '!', '  ']
+    for i in new_list:
+        if i == '  ':
+            text_list = text.replace(i, ' ')
+        else: text_list = text.replace(i, '')
+        text = text_list
+    text2 =text.split(' ')
+    return len(text2)
 
 
 text = "dsad! sadad, - dsada sdad sda sdadas – dsad, dsadad. dsada, sda"
