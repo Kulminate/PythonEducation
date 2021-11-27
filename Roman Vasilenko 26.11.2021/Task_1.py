737b2d2fdc06661d03d7a14630e90ad5b1997288
Roman Vasilenko <romanvas314@gmail.com>
2021-11-28 01:24:30 +0200

    Task 1 changed

--- Roman Vasilenko 26.11.2021/Task_1.py
+++ Roman Vasilenko 26.11.2021/Task_1.py
@@ -2,22 +2,21 @@
 # построчно последние строки в количестве lines.
 # Написать несколько вызовов метода как для позитивных и негативных вариантов. Функция должна обрабатывать все ситуации
 
-def read_last(file: str = 'article.txt', lines: int = 0):
+def read_last(enter_file: str = 'article.txt', use_lines: int = 0):
     try:
-        with open(f'{file}', 'r', encoding="utf-8") as use_file:
+        with open(f'{enter_file}', 'r', encoding="utf-8") as use_file:
             data_list = use_file.readlines()
 
-        if 0 <= lines <= len(data_list):
-            for i in range(1, lines+1):
+        if 0 <= use_lines <= len(data_list):
+            for i in range(1, use_lines + 1):
                 print(data_list[-i], end="")
         else:
-            return print("Enter valid lines values. Must be between null and count lines of file")
+            print("Enter valid lines values. Must between null and count lines of file")
 
     except FileNotFoundError as traceback:
         print(f"File not found. Traceback: {traceback}")
 
 
-
 lines = int(input('Enter lines: '))
 file = input('Enter file (try article.txt): ')
-read_last(lines=lines, file=file)
+read_last(use_lines=lines, enter_file=file)
