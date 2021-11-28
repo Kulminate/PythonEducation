c58226553e182f20873e4946fa7df16cd878ba95
Roman <roman.nenakhov96@gmail.com>
2021-11-28 22:20:15 +0200

    python hw4 task4

--- Roman Nenakhov/python_hw4/task4.py
+++ Roman Nenakhov/python_hw4/task4.py
@@ -1,14 +1,20 @@
 # Написать метод isDigit, который возращает указанная строка число или нет.
-#
-# Примеры, в которых метод должен возвращать true
-# isDigit("3")
-# isDigit("  3  ")
-# isDigit("-3.23")
-#
-# Примеры, в которых метод должен возвращать false
-# isDigit("3-4")
-# isDigit("  3   5")
-# isDigit("3 5")
-# isDigit("zero")
-# Вызовы методов с указанными как позитивными так и негативными случаями
-# сразу прописываем что бы мне было удобнее проверять
+
+
+def isDigit(line):
+    try:
+        float(line)
+        return True
+    except ValueError:
+        return False
+
+
+# True
+print(isDigit("3"))
+print(isDigit("  3  "))
+print(isDigit("-3.23"))
+# False
+print(isDigit("3-4"))
+print(isDigit("  3   5"))
+print(isDigit("3 5"))
+print(isDigit("zero"))
