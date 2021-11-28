087e461a4b99904df6849b346116ab547b65ded9
Y4rkiy <70848088+Y4rkiy@users.noreply.github.com>
2021-11-28 15:16:06 +0200

    task5

---
+++ Yaroslav Borodin 26.11.21/5.py
@@ -0,0 +1,28 @@
+# 5) Написать метод century(), который возвращает век,
+# в котором был этот год(века до н.е. тоже бывают)
+import math
+
+
+def century(era, year):
+    century = math.ceil(int(year) / 100)
+    res = str(century) + " век"
+    if era == "0":
+        res += " до н.э."
+    else:
+        res += " н.э."
+    return res
+
+
+correct_input = False
+while not correct_input:
+    input_era = input("Выберите хронологический период \n(0 - до н.э., 1 - н.э.)\n")
+    if input_era not in ("0", "1"):
+        print("Некорректный ввод эры, повторите ввод")
+        continue
+    input_year = input("Введите год \n(например: 2015, 3, 564)\n")
+    if not input_year.isdigit():
+        print("Некорректный ввод года, повторите ввод")
+        continue
+    correct_input = True
+
+print(century(input_era, input_year))
