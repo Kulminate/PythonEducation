b8db1b28e9b40f7e1ad999a9feb7ae7d6b412537
Medentsev <medentsevartem@gmail.com>
2021-11-29 19:08:38 +0200

    29.11\Task1 is added

---
+++ MedentsevArtem/29.11/Person/Person.py
@@ -0,0 +1,36 @@
+class Person():
+    def __init__(self, name, age, sex):
+        self.name = name
+        self.age = age
+        self.sex = sex
+
+    def __str__(self):
+        return f'Привет, я {self.sex}, меня зовут {self.name}, мне {self.age} лет.'
+
+    def __del__(self):
+        print(f'До свидания, мистер {self.name}.')
+
+    @property
+    def sex(self):
+        return self.__sex
+
+    @sex.setter
+    def sex(self, s):
+        if s.lower() == 'м' or s.lower() == 'мужской' or s.lower() == 'мужчина':
+            self.__sex = 'мужчина'
+        elif s.lower() == 'ж' or s.lower() == 'женский' or s.lower() == 'женщина':
+            self.__sex = 'женщина'
+        else:
+            raise ValueError
+
+    @property
+    def age(self):
+        return self.__age
+
+    @age.setter
+    def age(self, a):
+        if a.isdigit():
+            if 16 <= int(a) <= 65:
+                self.__age = a
+            else:
+                raise ValueError
