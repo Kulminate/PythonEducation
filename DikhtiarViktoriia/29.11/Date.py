29c3071f368a5d7d084df7fec6c181e9fdbc8f6c
dihty <dihyarviktoriya95@gmail.com>
2021-11-29 14:35:00 +0200

    Date.py changed

--- DikhtiarViktoriia/29.11/Date.py
+++ DikhtiarViktoriia/29.11/Date.py
@@ -1,37 +1,37 @@
 import datetime
 
 class Date:
-    def __init__(self, year, month, day):
-        self.__year = year
-        self.__month = month
-        self.__day = day
+    def __init__(self, year: int, month: int, day: int):
+        self.year = year
+        self.month = month
+        self.day = day
 
     def __str__(self):
-        return (f"{self.__year}, {self.__month}, {self.__day}")
+        return (f"{self._year}, {self._month}, {self._day}")
 
     @property
     def year(self):
-        return self.__year
+        return self._year
 
     @year.setter
-    def set_year(self, y):
-        self.__year = y
+    def year(self, y: int):
+        self._year = y
 
     @property
     def month(self):
-        return self.__month
+        return self._month
 
     @month.setter
-    def set_month(self, m):
-        self.__month = m
+    def month(self, m: int):
+        self._month = m
 
     @property
     def day(self):
-        return self.__day
+        return self._day
 
     @day.setter
-    def set_day(self, d):
-        self.__day = d
+    def day(self, d: int):
+        self._day = d
 
     def checkData(self, year: int, month: int, day: int):
         try:
@@ -40,12 +40,30 @@ class Date:
             print(e)
 
     def differenceIdDays(self, year: int, month: int, day: int):
-        return (self.checkData(self.__year, self.__month, self.__day) - datetime.date(year, month, day)).days
+        return (self.checkData(self._year, self._month, self._day) - datetime.date(year, month, day)).days
 
 
 
 d = Date(2012, 5, 23)
-print(d.checkData(2012, 5, 23))
 print(d.differenceIdDays(2012, 5, 10))
 
 
+# class Date:
+#     def __init__(self, day: int):
+#         self._day = day
+#
+#     def get_day(self):
+#         return self._day
+#
+#     def set_day(self, day: int):
+#         Date.checkData()
+#         self._day = day
+#
+#     @staticmethod
+#     def checkData():
+#         pass
+#
+# d = Date(1)
+# d.set_day()
+# print(d.get_day())
+# print()
