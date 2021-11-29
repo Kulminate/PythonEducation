7765ad15ea4f12cad28f416837055c88da98c6ca
Y4rkiy <70848088+Y4rkiy@users.noreply.github.com>
2021-11-30 00:27:38 +0200

    task4

---
+++ Yaroslav Borodin 29.11.21/4/Date.py
@@ -0,0 +1,57 @@
+import datetime
+
+
+class Date:
+
+    def __init__(self, day, month, year):
+        self.day = day
+        self.month = month
+        self.year = year
+
+    @staticmethod
+    def checkData(day=9, month=12, year=1999):
+        try:
+            datetime.date(year, month, day)
+        except ValueError:
+            print("Некорректная дата")
+            return False
+        else:
+            return True
+
+    def differenceIdDays(self, day: int, month: int, year: int):
+        self_td = datetime.timedelta(self.year, self.month, self.day)
+        other_td = datetime.timedelta(year, month, day)
+        return (self_td-other_td).days
+
+    @property
+    def day(self):
+        return self.__day
+
+    @property
+    def year(self):
+        return self.year
+
+    @property
+    def month(self):
+        return self.month
+
+    @day.setter
+    def day(self, day):
+        if Date.checkData(day=day):
+            self.__day = day
+        else:
+            raise ValueError
+
+    @year.setter
+    def year(self, year):
+        if Date.checkData(year=year):
+            self.__year = year
+        else:
+            raise ValueError
+
+    @month.setter
+    def month(self, month):
+        if Date.checkData(month=month):
+            self.__month = month
+        else:
+            raise ValueError
---
+++ Yaroslav Borodin 29.11.21/4/main.py
@@ -0,0 +1,5 @@
+from Date import Date
+
+if __name__ == "__main__":
+    date1 = Date(9, 12, 1999)
+    print(date1.differenceIdDays(5, 6, 2001))
