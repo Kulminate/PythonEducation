eadc06f54af076a785a37bdaa5f0b00e92f0946b
Roman Vasilenko <romanvas314@gmail.com>
2021-11-29 21:15:31 +0200

    Task 3 completed

---
+++ Roman Vasilenko 29.11.2021/Task_3/SomeNumber.py
@@ -0,0 +1,29 @@
+class SomeNumber:
+
+    def __init__(self, n: int = 0):
+        self.n = n
+
+    @property
+    def n(self):
+        return self.__n
+
+    @n.setter
+    def n(self, n):
+        self.__n = n
+
+    def getN(self):
+        return int(self.__n)
+
+    def setN(self):
+        return None
+
+    def print(self):
+        return print(f'Number is {self.__n}')
+
+    def isPositive(self):
+        if self.__n > 0:
+            return 'positive'
+        elif self.__n < 0:
+            return 'negative'
+        elif self.__n == 0:
+            return 'null'
---
+++ Roman Vasilenko 29.11.2021/Task_3/TestSomeNumber.py
@@ -0,0 +1,53 @@
+from SomeNumber import SomeNumber
+
+class TestSomeNumber:
+
+    # def __init__(self):
+    # # self.number = test_number
+    # @property
+    # def number(self):
+    #     return self.__number
+    #
+    # @number.setter
+    # def number(self, number):
+    #     self.__number = number
+
+    test_list: list = (-10, -1, 0, 1, 15)
+
+    @staticmethod
+    def test_setN():
+        test_list = (-10, -1, 0, 1, 15)
+        result_list = []
+        for i in test_list:
+            if SomeNumber.setN(SomeNumber(i)) == None:
+                result_list.append(f'Test number: {i}. Returned None. Test passed')
+
+            else:
+                result_list.append(f'Test number: {i}. Returned {SomeNumber.setN(i)}. Test failed')
+        print(f'Test list is {test_list}')
+        for result in result_list:
+            print(result)
+
+    @staticmethod
+    def test_is_positive():
+        test_list = (-10, -1, 0, 1, 15)
+        result_list = []
+        for i in test_list:
+            if SomeNumber.isPositive(SomeNumber(i)) == 'positive' and i > 0 \
+            or SomeNumber.isPositive(SomeNumber(i)) == 'negative' and i < 0 \
+            or SomeNumber.isPositive(SomeNumber(i)) == 'null' and i == 0:
+                result_list.append(f'Test number: {i}. Returned {SomeNumber.isPositive(SomeNumber(i))}. Test passed')
+
+            else:
+                result_list.append(f'Test number: {i}. Returned {SomeNumber.isPositive(SomeNumber(i))}. Test failed')
+        print(f'Test list is {test_list}')
+        for result in result_list:
+            print(result)
+
+
+
+
+
+
+
+
