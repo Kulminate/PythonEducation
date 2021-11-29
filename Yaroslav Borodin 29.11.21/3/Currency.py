fa6c62c33ec391f4dac7c6964ae8e34762df6758
Y4rkiy <70848088+Y4rkiy@users.noreply.github.com>
2021-11-29 23:50:28 +0200

    task3a

---
+++ Yaroslav Borodin 29.11.21/3/Currency.py
@@ -0,0 +1,45 @@
+class Currency:
+
+    def __init__(self, qty: float, name: str):
+        self.qty = qty
+        self.name = name
+
+    def print(self):
+        print(f"{self.qty:.2f} {self.name}")
+
+    def isEqual(self, curr):
+        if self.qty == curr.qty and self.name == curr.name:
+            return True
+        else:
+            return False
+
+    def __add__(self, other):
+        if self.name == other.name:
+            return self.qty+other.qty
+        else:
+            print("Error: currency names are differ 'грн' - 'руб")
+
+    def __sub__(self, other):
+        if self.name == other.name:
+            return self.qty - other.qty
+        else:
+            print("Error: currency names are differ 'грн' - 'руб")
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        if name.isalpha():
+            self.__name = name
+        else:
+            raise ValueError
+
+    @property
+    def qty(self):
+        return self._qty
+
+    @qty.setter
+    def qty(self, qty):
+        self._qty = qty
