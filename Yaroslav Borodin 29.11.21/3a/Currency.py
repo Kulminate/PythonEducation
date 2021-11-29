3bfef37febc5eca65db97eb2e0f02a554643696e
Y4rkiy <70848088+Y4rkiy@users.noreply.github.com>
2021-11-30 00:57:06 +0200

    tasks3a

--- Yaroslav Borodin 29.11.21/3a/Currency.py
+++ Yaroslav Borodin 29.11.21/3a/Currency.py
@@ -1,14 +1,14 @@
 class Currency:
 
     def __init__(self, qty: float, name: str):
-        self.qty = qty
-        self.name = name
+        self.__qty = qty
+        self.__name = name
 
     def print(self):
-        print(f"{self.qty:.2f} {self.name}")
+        print(f"{self.__qty:.2f} {self.__name}")
 
     def isEqual(self, curr):
-        if self.qty == curr.qty and self.name == curr.name:
+        if self.__qty == curr.qty and self.__name == curr.name:
             return True
         else:
             return False
@@ -38,8 +38,8 @@ class Currency:
 
     @property
     def qty(self):
-        return self._qty
+        return self.__qty
 
     @qty.setter
     def qty(self, qty):
-        self._qty = qty
+        self.__qty = qty
