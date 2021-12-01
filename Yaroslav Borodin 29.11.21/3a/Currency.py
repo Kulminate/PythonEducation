1ac0c6f56074b8774cb873f61edaac3bba6bf9d9
Y4rkiy <70848088+Y4rkiy@users.noreply.github.com>
2021-12-01 15:13:55 +0200

    refactored task3a

--- Yaroslav Borodin 29.11.21/3a/Currency.py
+++ Yaroslav Borodin 29.11.21/3a/Currency.py
@@ -1,45 +1,45 @@
 class Currency:
 
-    def __init__(self, qty: float, name: str):
+    def __init__(self, qty: float, name: str) -> None:
         self.__qty = qty
         self.__name = name
 
-    def print(self):
+    def print(self) -> None:
         print(f"{self.__qty:.2f} {self.__name}")
 
-    def isEqual(self, curr):
+    def isEqual(self, curr) -> bool:
         if self.__qty == curr.qty and self.__name == curr.name:
             return True
         else:
             return False
 
-    def __add__(self, other):
+    def __add__(self, other) -> int:
         if self.name == other.name:
-            return self.qty+other.qty
+            return self.qty + other.qty
         else:
             print(f"Error: currency names are differ '{self.name}' - '{other.name}'")
 
-    def __sub__(self, other):
+    def __sub__(self, other) -> float:
         if self.name == other.name:
             return self.qty - other.qty
         else:
             print(f"Error: currency names are differ '{self.name}' - '{other.name}'")
 
     @property
-    def name(self):
+    def name(self) -> str:
         return self.__name
 
     @name.setter
-    def name(self, name):
+    def name(self, name: str) -> None:
         if name.isalpha():
             self.__name = name
         else:
             raise ValueError
 
     @property
-    def qty(self):
+    def qty(self) -> float:
         return self.__qty
 
     @qty.setter
-    def qty(self, qty):
+    def qty(self, qty: float) -> None:
         self.__qty = qty
