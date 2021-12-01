15ca38448d4bc1b49fc18281c68f047abd9f59b8
Medentsev <medentsevartem@gmail.com>
2021-12-01 14:12:51 +0200

    Date is refactored

--- MedentsevArtem/29.11/Date(task4)/Date.py
+++ MedentsevArtem/29.11/Date(task4)/Date.py
@@ -14,64 +14,62 @@
 # 		- протестировать работу методов
 from datetime import timedelta
 class Date():
-    def __init__(self, d, m, y):
-        self.d = d
-        self.m = m
+    def __init__(self, y: int, m: int, d: int):
         self.y = y
-
-    @property
-    def d(self):
-        return self.__d
-
-    @d.setter
-    def d(self, d):
-        self.__d = d
-
-    def get_d(self):
-        return self.__d
-
-    @property
-    def m(self):
-        return self.__m
-
-    @m.setter
-    def m(self, m):
-        self.__m = m
-
-    def get_m(self):
-        return self.__m
+        self.m = m
+        self.d = d
 
     @property
     def y(self):
-        return self.__y
+        return self._y
 
     @y.setter
-    def y(self, y):
-        self.__y = y
+    def y(self, y: int):
+        if self.checkData(y):
+            self._y = y
 
-    def set(self, d, m, y):
-        if self.checkData(d, m, y):
-            return True
-        else:
-            raise ValueError('incorrect data')
+    @property
+    def m(self):
+        return self._m
 
-    def get_y(self):
-        return self.__y
+    @m.setter
+    def m(self, m: int):
+        if self.checkData(m):
+            self._m = m
+
+    @property
+    def d(self):
+        return self._d
+
+    @d.setter
+    def d(self, d: int):
+        if self.checkData(d):
+            self._d = d
 
     @staticmethod
-    def checkData(self) -> int:
-        if not self.d or not self.m or not self.y:
+    def checkData(d=None, m=None, y=None) -> int:
+        if not d and not m and not y:
             raise UnboundLocalError('Not enough data.')
         else:
-            return True
+            if d is not None and type(d) != Date:
+                if d > 31:
+                    raise ValueError('Incorrect data.')
+            if m is not None:
+                if m > 12:
+                    raise ValueError('Incorrect data.')
+            if y is not None:
+                if y > 2200:
+                    raise ValueError('Incorrect data.')
+            else:
+                return True
 
-    def differenceIdDays(self, other):
+    def differenceIdDays(self, other) -> str:
         if self.checkData(self) and other.checkData(other):
-            a = timedelta(days=(self.d+self.m*30+self.y*365)) - timedelta(days=(other.d+other.m*30+other.y*365))
+            a = timedelta(days=(self._d+self._m*30+self._y*365)) - timedelta(days=(other._d+other._m*30+other._y*365))
             time = a.days
             print(time)
             yy = time//365
             time -= yy*365
             mm = time//31
             time -= mm*31
-            return f'{yy} г. {mm} м. {time} д.'
+            return f'{yy} г., {mm} м., {time} д.'
--- MedentsevArtem/29.11/Date(task4)/DateTest.py
+++ MedentsevArtem/29.11/Date(task4)/DateTest.py
@@ -2,6 +2,6 @@ from Date import Date
 
 
 if __name__ == "__main__":
-    a = Date(1, 1, 1)
+    a = Date(1, 1, 31)
     b = Date(2, 2, 2)
     print(b.differenceIdDays(a))
