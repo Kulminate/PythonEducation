9c82ce07985bbc90dd7a28a2ea0160501f409f46
Y4rkiy <70848088+Y4rkiy@users.noreply.github.com>
2021-12-01 16:19:35 +0200

    task4 fixed and refactored

--- Yaroslav Borodin 29.11.21/4/Date.py
+++ Yaroslav Borodin 29.11.21/4/Date.py
@@ -3,54 +3,60 @@ import datetime
 
 class Date:
 
-    def __init__(self, day, month, year):
-        self.day = day
-        self.month = month
-        self.year = year
+    def __init__(self, day: int, month: int, year: int) -> None:
+        self.__day = day
+        self.__month = month
+        self.__year = year
 
     @staticmethod
-    def checkData(day=9, month=12, year=1999):
+    def checkData(day=9, month=12, year=1999) -> bool:
         try:
             datetime.date(year, month, day)
         except ValueError:
             print("Некорректная дата")
             return False
         else:
+            print("Корректная дата")
             return True
 
-    def differenceIdDays(self, day: int, month: int, year: int):
-        self_td = datetime.timedelta(self._year, self._month, self._day)
+    # - ** реализовать
+    # метод differenceIdDays(day: int, month: int, year: int), который принимает другую
+    # дату и вычисляет разницу в днях между датами
+    def differenceIdDays(self, day: int, month: int, year: int) -> datetime:
+        self_td = datetime.timedelta(self.__year, self.__month, self.__day)
+        print(self_td)
         other_td = datetime.timedelta(year, month, day)
-        return (self_td-other_td).days
+        print(other_td)
+        return (self_td - other_td).days
 
     @property
-    def day(self):
+    def day(self) -> int:
         return self._day
 
     @property
-    def year(self):
+    def year(self) -> int:
         return self._year
 
     @property
-    def month(self):
+    def month(self) -> int:
         return self._month
 
     @day.setter
-    def day(self, day):
+    def day(self, day: int) -> None:
         if Date.checkData(day=day):
             self._day = day
         else:
             raise ValueError
 
     @year.setter
-    def year(self, year):
+    def year(self, year: int) -> None:
         if Date.checkData(year=year):
             self._year = year
         else:
             raise ValueError
 
     @month.setter
-    def month(self, month):
+    def month(self, month: int) -> None:
         if Date.checkData(month=month):
             self._month = month
         else:
--- Yaroslav Borodin 29.11.21/4/main.py
+++ Yaroslav Borodin 29.11.21/4/main.py
@@ -1,8 +1,10 @@
+import datetime
+
 from Date import Date
-from TestDate import TestDate
 
 if __name__ == "__main__":
     date1 = Date(9, 12, 1999)
+    print(Date.checkData(year=2000, day=15, month=5))
     print(date1.differenceIdDays(5, 6, 2001))
-    TestDate.test()
+    print(datetime.timedelta(year=2005, month=2, day=19))
 
