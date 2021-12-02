1e0d412df00f0aa44df2c107f8d851aa93196e17
Roman <roman.nenakhov96@gmail.com>
2021-12-02 22:30:12 +0200

    python hw6 task2

--- Roman Nenakhov/python_hw6/task2.py
+++ Roman Nenakhov/python_hw6/task2.py
@@ -3,3 +3,51 @@
 # метод say(), который бы возвращал звук, издаваемый этим животным.Создайте
 # кортеж из нескольких этих экземпляров классов, переберите их в цикле и
 # выведите в консоль их звуки(вызовите метод say()).
+
+
+class Animal:
+
+    def say(self):
+        print('speaks in his language')
+
+
+class Dog(Animal):
+
+    def say(self):
+        print('Woof!!!!')
+
+
+class Cat(Animal):
+
+    def say(self):
+        print('Meow!!!!')
+
+
+class Frog(Animal):
+
+    def say(self):
+        print('Croak!!!')
+
+
+class Cow(Animal):
+
+    def say(self):
+        print('Moooooooo!!!')
+
+
+class Fly(Animal):
+
+    def say(self):
+        print('Bzzzzzzzzz')
+
+
+dog = Dog()
+cat = Cat()
+frog = Frog()
+cow = Cow()
+fly = Fly()
+
+animals = dog, cat, frog, cow, fly
+
+for animal in animals:
+    animal.say()
